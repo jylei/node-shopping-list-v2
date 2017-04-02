e6b0e4c6d66f73875996c2c7788f5fc418f186de
Jun Lei <Jun Lei>
2017-04-02 01:30:01 -0700

    added recipes POST

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+})
+
 app.post('/shopping-list', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'budget'];
@@ -42,14 +46,23 @@ app.post('/shopping-list', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-
   const item = ShoppingList.create(req.body.name, req.body.budget);
   res.status(201).json(item);
 });
 
-
-app.get('/recipes', (req, res) => {
-  res.json(Recipes.get());
+app.post('/recipes', jsonParser, (req,res) => {
+  //ensure .... are in req body
+  const requiredFields = ['name', 'ingredients'];
+  for(let i = 0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
 })
 
 app.listen(process.env.PORT || 8080, () => {
